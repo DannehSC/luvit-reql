8269bbe9911968f19a6ad6cdee780ae7eb307b1f
Nameless <truemedian@gmail.com>
2018-10-01 18:02:04 -0500

    ReqlError rewrite
    + calculates hierarchy once and separately from the constructor
    + adds more efficiency to returning error and formatting said error.

--- libs/error.lua
+++ libs/error.lua
@@ -1,5 +1,5 @@
 
-local ReqlError = {}
+local ReqlError = { }
 
 local hierarchy = {
 	ReqlCompileError = 'ReqlError',
@@ -18,44 +18,48 @@ local hierarchy = {
 	ReqlOpIndeterminateError = 'ReqlAvailabilityError'
 }
 
+function calculateHierarchy(str)
+    local previous = hierarchy[str]
+
+    if type(previous) == 'string' then
+        previous = calculateHierarchy(previous)
+    else
+        return { str }
+    end
+
+    previous[#previous + 1] = str
+
+    hierarchy[str] = previous
+    return previous
+end
+
+for k in pairs(hierarchy) do calculateHierarchy(k) end
+for k, v in pairs(hierarchy) do hierarchy[k] = table.concat(v, '/') end
+
 local format = string.format
 
-local errMeta = {
-	__tostring = function(t)
-		return t:init()
-	end
-}
-
+local metatable = { __tostring = function(self) return self.message end }
 function ReqlError:__index(index)
-	return function(msg, term, frames)
-		local err = setmetatable({
-			name = index,
-			message = msg,
-			term = term,
-			frames = frames
-		}, errMeta)
-		function err:init()
-			local h = ''
-			local last = self.name
-			while last do
-				if hierarchy[last] then
-					h = format('%s/%s', hierarchy[last], h)
-					last = hierarchy[last]
-				else
-					last = nil
-				end
-			end
-			local message = format('%s%s || %s', h, self.name, self.message)
-			if self.term then
-				self.frames = type(self.frames) == 'table' and self.frames or {}
-				message = format('%s in:\n\t%s\n\t%s\n', message, self.term, table.concat(frames, ',\n\t'))
-			end
-			return message
-		end
-		return err
+    local path = hierarchy[index]
+
+    return function(message, term, frames)
+        if term then
+            frames = type(frames) == 'table' and frames or { }
+            
+            message = ('%s | %s in:\n\t%s\n\t%s\n'):format(path, message, term, table.concat(frames, ',\n\t'))
+        else
+            message = ('%s | %s'):format(path, message)
+        end
+            
+		return setmetatable({
+            name = index,
+            path = path,
+
+			message = message,
+			term    = term,
+            frames  = frames
+        }, metatable)
 	end
 end
 
-ReqlError = setmetatable({}, ReqlError)
-
-return ReqlError
+return setmetatable({ }, ReqlError)
