dbfa2780a4c298c0a4839aa63054a33c49dd03c8
DannehSC <dangerspookycanyon@gmail.com>
2019-12-07 17:32:00 -0600

    bump

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
 	name = "DannehSC/luvit-reql",
-	version = "1.0.7",
+	version = "1.0.8",
 	description = "A rethinkdb driver for Luvit, please send me a message if you need any assistance or extra features not currently present.",
 	tags = { "luvit", "rethinkdb", "database", "driver" },
 	license = "MIT",
